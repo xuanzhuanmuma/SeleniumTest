747754ad00c8175fe4e53ff569d99d6686880ca5
xuanzhuanmuma <github13579>
2020-04-12 12:27:44 +0800

    fix

---
+++ src/main/java/com/example/project1/Test1.java
@@ -0,0 +1,15 @@
+package com.example.project1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void testCase1() {
+        System.out.println("你好啊");
+    }
+
+    @Test
+    public void testCase2() {
+        System.out.println("你好啊111");
+    }
+}
